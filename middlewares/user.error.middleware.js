684b6f9c26de94b8f25d62d181f6fdc4a31dfb95
kolia5 <nikshevtsov5@gmail.com>
2021-12-25 16:19:58 +0200

    user.error.middleware changed

--- middlewares/user.error.middleware.js
+++ middlewares/user.error.middleware.js
@@ -1,10 +1,11 @@
+//these middlewares i use if i have got result from db without errors
+// but if Boolean(data) == false i send error object to responseMiddleware
+
 const getAllUsersMiddleware = (req, res, next) => {
   const data = res.data;
   if(!res.err && !data){
-    res.err = {
-      status: 404,
-      message: 'Users are not found'
-    }
+    res.err = { status: 404, message: 'Users are not found' };
+    res.data = null
   }
   next();
 }
@@ -12,10 +13,8 @@ const getAllUsersMiddleware = (req, res, next) => {
 const getUserMiddleware = (req, res, next) => {
   const data = res.data;
   if(!res.err && !data){
-    res.err = {
-      status: 404,
-      message: 'User with this ID is not found'
-    }
+    res.err = { status: 404,  message: 'User with this ID is not found' };
+    res.data = null;
   }
   next();
 }
